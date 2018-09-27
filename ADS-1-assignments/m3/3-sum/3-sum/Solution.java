43971a83681ea9ae651c433bdd0ea9b52ad4c7ee
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-09-27 19:22:18 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1-assignments/m3/3-sum/3-sum/Solution.java
+++ ADS-1-assignments/m3/3-sum/3-sum/Solution.java
@@ -3,36 +3,54 @@ import java.util.Arrays;
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * reads the input from the file and insert the elements into array
-	 * and checks the number of possibilities 3sum from the elements 
-	 * in array.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		/**
-		 * we initialise the array here.
-		 */
-		int arr[];
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int i = 0;
-		arr = new int[n];
-		while (i < n) {
-			arr[i] = sc.nextInt();
-			i++;
-		}
-		Arrays.sort(arr);
-		int count = 0;
-		for (int k = 0; k < arr.length; k++) {
-			for (int j = arr.length - 1; j > k + 1; j--) {
-				if ((arr[k] + arr[k + 1] + arr[j]) == 0) {
-					count += 1;
-				}
-			}
-		}
-		System.out.println(count);
-	}
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * { main method to read the input }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int sizeofarray = scan.nextInt();
+        int key;
+        int[] array = new int[sizeofarray];
+        for (int i = 0; i < sizeofarray; i++) {
+            array[i] = scan.nextInt();
+        }
+        System.out.println(threesum(array));
+    }
+    /**
+     * { To find the no of occurences of 3 sum problem }.
+     *
+     * @param      array  The array is of integer type
+     *
+     * @return     { Count is returned }
+     */
+    public static int threesum(final int[] array) {
+        int count = 0;
+        int k = 0, j = 0;
+        Arrays.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            j = i + 1;
+            k = array.length - 1;
+            while (j < k) {
+                if (array[i] + array[j] + array[k] == 0) {
+                    count++;
+                    j++;
+                    k--;
+                } else if (array[i] + array[j] + array[k] > 0) {
+                    k--;
+                } else if (array[i] + array[j] + array[k] < 0) {
+                    j++;
+                }
+            }
+        }
+        return count;
+    }
 }
