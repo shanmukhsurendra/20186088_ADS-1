61a429445e3c576958fea7d90551cbfe8a291165
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-09-28 18:50:59 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS-1-assignments/m5/5.1 Deque/5.1 Deque/Solution.java
+++ ADS-1-assignments/m5/5.1 Deque/5.1 Deque/Solution.java
@@ -3,44 +3,46 @@ import java.util.Scanner;
  * Class for solution.
  */
 class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * reads the input and calls the recquired method.
-	 *
-	 * @param      args  The arguments.
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		LinkedList list = new LinkedList();
-		int choice = scan.nextInt();
-		//int k = 1;
-		while (scan.hasNext()) {
-			String ch = scan.nextLine();
-			String[] arra = ch.split(" ");
-			switch (arra[0]) {
-			case "pushLeft":
-				list.pushLeft(Integer.parseInt(arra[1]));
-				break;
-			case "pushRight":
-				list.pushRight(Integer.parseInt(arra[1]));
-				break;
-			case "size":
-				list.size();
-				break;
-			case "popRight":
-				list.popRight();
-				break;
-			case "popLeft":
-				list.popLeft();
-				break;
-			default:
-				break;
-			}
-		}
-	}
+    }
+    /**
+     * reads the input and calls the recquired method.
+     *
+     * @param      args  The arguments.
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        LinkedList list = new LinkedList();
+        int choice = scan.nextInt();
+        //int k = 1;
+        while (scan.hasNext()) {
+            String ch = scan.nextLine();
+            String[] arra = ch.split(" ");
+            switch (arra[0]) {
+            case "pushLeft":
+                list.pushLeft(Integer.parseInt(arra[1]));
+                break;
+            case "pushRight":
+                list.pushRight(Integer.parseInt(arra[1]));
+                break;
+            case "size":
+                list.size();
+                break;
+            case "popRight":
+                list.popRight();
+                break;
+            case "popLeft":
+                list.popLeft();
+                break;
+            default:
+                break;
+            }
+        }
+    }
 }
+
+
