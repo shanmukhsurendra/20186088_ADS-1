963895148f73d60b6f792e64ae238bb07e9d8ded
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-09-29 11:47:07 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- ADS-1-assignments/m6/Assignment-1/Assignment-1/Solution.java
+++ ADS-1-assignments/m6/Assignment-1/Assignment-1/Solution.java
@@ -1,170 +1,88 @@
 import java.util.Scanner;
 class AddLargeNumbers {
-	//LinkedList li = new LinkedList();
-	// private class Node {
-	// 	int data;
-	// 	Node next;
-	// }
-	// Node head, tail;
-//    public void addAtHead(int data) {
-	// 	Node node = new Node();
-	// 	node.data = data;
-	// 	node.next = head;
-	// 	if (head == null) tail = node;
-	// 	head = node;
-	// }
-	public static LinkedList numberToDigits(String number) {
-		// for (int i = 0, n = number.length(); i < n; i++) {
-		// 	char c = number.charAt(i);
-		// 	int a = Integer.parseInt(c);
-		// 	li.addAtHEad(Integer.parseInt(a));
-		// }
-		// char[] chars = number.toCharArray();
-		// for (int i = 0, n = chars.length; i < n; i++) {
-		// 	int c = Integer.parseInt(chars[i]);
-		// }
-		// return li.print();
-		// LinkedList l = new LinkedList();
-		LinkedList li = new LinkedList();
-		String[] arra = number.split("");
-		for (int i = 0; i < arra.length; i++) {
-			li.addAtHead(arra[i]);
-		}
-		//l.printList();
-		return li;
-	}
 
-	public static String digitsToNumber(LinkedList list) {
-		 String str = "";
-        while(!list.isEmpty()) {
-            str += list.popAtTail();
+    public static LinkedList numberToDigits(String number) {
+        LinkedList list = new LinkedList();
+        for (int i = 0; i < number.length(); i++) {
+            char digit = number.charAt(i);
+            list.push(Integer.parseInt(String.valueOf(digit)));
         }
-        return str;
+        return list;
     }
-	
 
-	public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
-		return new LinkedList();
-	}
-	//   public void print() {
-	// Node thead = head;
-	// while(thead != null) {
-	// 	System.out.println(thead.data);
-	// 	thead = thead.next;
-	// }
+    public static String digitsToNumber(LinkedList list) {
+        String number = list.print();
+        return number;
+    }
 
+    public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
+        return list1;
+    }
 }
 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String input = sc.nextLine();
-		String p = sc.nextLine();
-		String q = sc.nextLine();
-		switch (input) {
-		case "numberToDigits":
-			LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
-			LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
-			System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
-			System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
-			break;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String p = sc.nextLine();
+        String q = sc.nextLine();
+        switch (input) {
+        case "numberToDigits":
+            LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
+            LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
+            System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
+            System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
+            break;
 
-		case "addLargeNumbers":
-			pDigits = AddLargeNumbers.numberToDigits(p);
-			qDigits = AddLargeNumbers.numberToDigits(q);
-			LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
-			System.out.println(AddLargeNumbers.digitsToNumber(result));
-			break;
-		}
-	}
+        case "addLargeNumbers":
+            pDigits = AddLargeNumbers.numberToDigits(p);
+            qDigits = AddLargeNumbers.numberToDigits(q);
+            LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
+            System.out.println(AddLargeNumbers.digitsToNumber(result));
+            break;
+        }
+    }
 
 }
 class LinkedList {
-	private class Node {
-		String data;
+	Node head;
+	public  class Node {
+		int data;
 		Node next;
-	}
-		int size = 0;
-	Node head, tail;
-	public void addAtHead(String data) {
-		Node node = new Node();
-		node.data = data;
-		node.next = head;
-		if (head == null) tail = node;
-		head = node;
-		size++;
-	}
-	public boolean isEmpty() {
-		return size == 0;
-	}
-	public void addAtTail(String data) {
-		if (tail == null) {
-			addAtHead(data);
-			return;
+		Node(int data) {
+			this.data=data;
+
 		}
-		Node node = new Node();
-		node.data = data;
-		tail.next = node;
-		tail = tail.next;
-		size++;
 	}
+	public  void push(int data) {
+		Node node = new Node(data);
+		node.next=null;
 
-	public void deleteAtHead() {
-		head = head.next;
-		size--;
+		if(head==null) {
+			head = node;
+		} else {
+			Node thead = head;
+			while(thead.next!=null) {
+				thead=thead.next;
+			}
+			thead.next=node;
+		}
 	}
-
-	public String popAtHead() {
-		String data = head.data;
-		head = head.next;
-		size--;
-		return data;
-	}
-
-	public void deleteAtTail() {
+	public void pop(int i) {
 		Node thead = head;
-		while (thead.next != tail)
-			thead = thead.next;
-		thead.next = null;
-		tail = thead;
-		size--;
-	}
-
-	public String popAtTail() {
-		String data = tail.data;
-		Node thead = head;
-		while (thead.next != tail)
-			thead = thead.next;
-		thead.next = null;
-		tail = thead;
-		size--;
-		return data;
-	}
-
-	public void insertAfter(int index, String data) {
-		Node thead = head;
-		for (int i = 0; i < index; i++)
-			thead = thead.next;
-		Node node = new Node();
-		node.data = data;
-		node.next = thead.next;
-		thead.next = node;
-		size++;
-	}
-
-	public void removeAfter(int index) {
-		Node thead = head;
-		for (int i = 0; i < index; i++)
-			thead = thead.next;
-		thead.next = thead.next.next;
-		size--;
-	}
-
-	public void print() {
-		Node thead = head;
-		while (thead != null) {
-			System.out.println(thead.data);
+		int temp=1;
+		while(thead.next.next!=null) {
 			thead = thead.next;
 		}
+		thead.next = null;
+	}
+	public String print() {
+			Node thead = head;
+			String content = "";
+			while(thead !=null) {
+				content+=thead.data;
+				thead=thead.next;
+			}
+			return content;
 	}
 }
