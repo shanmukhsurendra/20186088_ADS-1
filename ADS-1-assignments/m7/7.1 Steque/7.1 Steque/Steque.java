95a134dc11d7b91ce878ee63d9941d3c1d668586
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-10-01 17:48:12 +0530

    "check_style_score -> 2 of 2 passed. style score: 1/1 "

--- ADS-1-assignments/m7/7.1 Steque/7.1 Steque/Steque.java
+++ ADS-1-assignments/m7/7.1 Steque/7.1 Steque/Steque.java
@@ -1,11 +1,23 @@
 import java.util.*;
 class Steque {
+	/**
+	 * Class for node.
+	 */
     private class Node {
         int data;
         Node next;
     }
     Node head, next, tail;
     int size = 0;
+    /**
+     * push method is used to push the elements into linked list from the 
+     * left side we can consider it as the top of the stack.
+     * Worst case: O(1)
+     * Best case: O(1)
+     * Average case: O(1)
+     *
+     * @param      data  The data
+     */
     public void push(int data) {
         Node node = new Node();
         node.data = data;
@@ -17,7 +29,15 @@ class Steque {
         //print();
         size++;
     }
-
+    /**
+     * enqueue is used to push the elements into linked list from the back 
+     * end.
+     * Worst case: O(1)
+     * Best case: O(1)
+     * Average case: O(1)
+     *
+     * @param      data  The data
+     */
     public void enqueue(int data) {
         if (head == null || tail == null) {
             push(data);
@@ -30,6 +50,13 @@ class Steque {
         }
         size++;
     }
+    /**
+     * it removes the element from the linked list and prints the remaining
+     * elements.
+     * Worst case: O(1)
+     * Best case: O(1)
+     * Average case: O(1)
+     */
     public void pop() {
         if (head == null) {
             //System.out.println("Steque is empty.");
@@ -43,8 +70,13 @@ class Steque {
         }
         size--;
     }
+    /**
+     * prints the elements in the linked list considering headf as the starting psotion.
+     * Worst case: O(N)
+     * Best case: O(N)
+     * Average case: O(N)
+     */
     public void print() {
-
         Node thead = head;
         try{
         while (thead != null && thead.next != null) {
