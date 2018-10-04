3d3214d685c1c1fd2a6f006fbdcc2e45ebb1a7a7
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-10-04 23:16:53 +0530

    "ads1_m10_a1: 1/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/Solution.java
+++ ADS-1-assignments/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/Solution.java
@@ -4,26 +4,26 @@ class Solution {
 		Scanner sc = new Scanner(System.in);
 
 		LinkedList li = new LinkedList();
-		// while (sc.hasNext()) {
-		// 	String str1 = sc.nextLine();
-		// String[] arr1 = str1.split(" ");
-		// 	switch (arr1[0]) {
-		// 	case "insertAt":
-		// 		li.insertAt(Integer.parseInt(arr1[1]), Integer.parseInt(arr1[2]));
-		// 		break;
-		// 	case "reverse":
-		// 		try{
-		// 		li.reverse();
-		// 		li.print();
-		// 	}
-		// 		catch (Exception e) {
-		//           		System.out.println("No elements to reverse.");
-		//           	}
-		// 		break;
-		// 	default:
-		// 		break;
-		// 	}
-		// }
+		while (sc.hasNext()) {
+			String str1 = sc.nextLine();
+		String[] arr1 = str1.split(" ");
+			switch (arr1[0]) {
+			case "insertAt":
+				li.insertAt(Integer.parseInt(arr1[1]), Integer.parseInt(arr1[2]));
+				break;
+			case "reverse":
+				try{
+				li.reverse();
+				li.print();
+			}
+				catch (Exception e) {
+		          		System.out.println("No elements to reverse.");
+		          	}
+				break;
+			default:
+				break;
+			}
+		}
 		// li.insertAt(0, 6);
 		// li.insertAt(0, 5);
 		// li.insertAt(1, 7);
@@ -32,17 +32,17 @@ class Solution {
 		// li.insertAt(-1, 9);
 		// li.insertAt(7, 9);
 		// li.reverse();
-		li.insertAt(0,5);
-		li.insertAt(0,7);
-		li.insertAt(1,9);
-			li.reverse();
-		li.insertAt(7,8);
-		li.insertAt(2,17);
-		li.insertAt(2,27);
-		li.insertAt(0,71);
-		li.insertAt(0,75);
-		//li.insertAt(0,75);
-		li.insertAt(5,66);
+		// li.insertAt(0,5);
+		// li.insertAt(0,7);
+		// li.insertAt(1,9);
+		// 	li.reverse();
+		// li.insertAt(7,8);
+		// li.insertAt(2,17);
+		// li.insertAt(2,27);
+		// li.insertAt(0,71);
+		// li.insertAt(0,75);
+		// //li.insertAt(0,75);
+		// li.insertAt(5,66);
 
 	}
 }
