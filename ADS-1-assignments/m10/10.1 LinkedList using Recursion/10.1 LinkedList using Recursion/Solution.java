a1e82773d2697a324b7d4d015258c5539b36e6cc
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-10-04 22:38:47 +0530

    "ads1_m10_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/Solution.java
+++ ADS-1-assignments/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/Solution.java
@@ -1,29 +1,30 @@
+import java.util.*;
 class Solution {
 	public static void main(String[] args) {
-		// Scanner sc = new Scanner(System.in);
-		// String str1 = sc.nextLine();
-		// String[] arr1 = str1.split(" ");
-		// while (sc.hasNext()) {
+		Scanner sc = new Scanner(System.in);
+		String str1 = sc.nextLine();
+		String[] arr1 = str1.split(" ");
 		 	LinkedList li = new LinkedList();
-		// 	switch (arr1[0]) {
-		// 	case "insertAt":
-		// 		li.insertAt(Integer.parseInt(arra1[1]), Integer.parseInt(arr1[2]));
-		// 		break;
-		// 	case "revberse":
-		// 		li.reverse();
-		// 		break;
-		// 	default:
-		// 		break;
-		// 	}
-		// }
-		li.insertAt(0, 6);
-		li.insertAt(0, 5);
-		li.insertAt(1, 7);
-		li.insertAt(2, 8);
-		li.insertAt(3, 9);
-		li.insertAt(-1, 9);
-		li.insertAt(7, 9);
-		li.reverse();
+		while (sc.hasNext()) {
+			switch (arr1[0]) {
+			case "insertAt":
+				li.insertAt(Integer.parseInt(arr1[1]), Integer.parseInt(arr1[2]));
+				break;
+			case "revberse":
+				li.reverse();
+				break;
+			default:
+				break;
+			}
+		}
+		// li.insertAt(0, 6);
+		// li.insertAt(0, 5);
+		// li.insertAt(1, 7);
+		// li.insertAt(2, 8);
+		// li.insertAt(3, 9);
+		// li.insertAt(-1, 9);
+		// li.insertAt(7, 9);
+		// li.reverse();
 
 	}
 }
