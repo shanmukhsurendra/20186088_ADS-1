cdb73f88edfa082aaeb6057dbb3bc5065d785cca
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-10-05 09:36:56 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/Solution.java
+++ ADS-1-assignments/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/Solution.java
@@ -1,4 +1,9 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+
+/**
+ * Class for solution.
+ */
 class Solution {
     /**
      * reads the input and calls the mnethods. 
