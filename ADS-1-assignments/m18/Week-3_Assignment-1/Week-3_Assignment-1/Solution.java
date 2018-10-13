8b70c4bfac51ecdfd883b63934f91ab13f9eab5f
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-10-13 10:54:30 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-assignments/m18/Week-3_Assignment-1/Week-3_Assignment-1/Solution.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int numberOfItems = sc.nextInt();
+		int i = 0;
+		while (i < numberOfItems) {
+			int j = 0;
+			while (j < 6) {
+				String[] arra1 = sc.nextLine().split(",");
+				System.out.println(arra1[1]);
+				j++;
+			}
+			i++;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public class Solution {
+// 	public static void main(String[] args) {
+// 		Scanner scan = new Scanner(System.in);
+// 		int n = Integer.parseInt(scan.nextLine());
+// 		while (n > 0) {
+// 			int hours = 6;
+// 			while (hours > 0) {
+// 				String[] line = scan.nextLine().split(",");
+// 				System.out.println(line[1]);
+// 				hours--;
+// 			}
+// 			System.out.println();
+// 			n--;
+// 		}
+// 	}
+// }
