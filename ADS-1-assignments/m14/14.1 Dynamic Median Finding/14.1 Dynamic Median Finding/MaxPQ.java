29991dc6c54d78d91ad44801ef293752a22ac25a
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-11-05 10:08:05 +0530

    module-14

--- ADS-1-assignments/m14/14.1 Dynamic Median Finding/14.1 Dynamic Median Finding/MaxPQ.java
+++ ADS-1-assignments/m14/14.1 Dynamic Median Finding/14.1 Dynamic Median Finding/MaxPQ.java
@@ -153,3 +153,19 @@ public class MaxPQ<Key> {
         pq[j] = swap;
     }
 }
+// public class Solution {
+//  public static void main(String[] args) {
+//      Scanner scan = new Scanner(System.in);
+//      int n = Integer.parseInt(scan.nextLine());
+//      while (n > 0) {
+//          int hours = 6;
+//          while (hours > 0) {
+//              String[] line = scan.nextLine().split(",");
+//              System.out.println(line[1]);
+//              hours--;
+//          }
+//          System.out.println();
+//          n--;
+//      }
+//  }
+// }
