0a0d02a728d5f80db92697ac852920188dce88a7
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2019-04-14 05:09:30 +0530

    "ads1_m7_a1: 0/2 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ urike/7.1 Steque/7.1 Steque/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Steque s = new Steque();
+		int i = 0;
+		while (i < n){
+			String inp = sc.nextLine();
+			String[] arr = inp.split(" ");
+			switch(arr[0]) {
+				case "push":
+					s.push(Integer.parseInt(arr[1]));
+					break;
+				case "pop":
+					s.pop();
+					break;
+				case "enqueue":
+					s.enqueue(Integer.parseInt(arr[1]));
+					break;
+				default:
+					break;
+
+			}
+		}
+	}
+}
---
+++ urike/7.1 Steque/7.1 Steque/Steque.java
@@ -0,0 +1,56 @@
+class Steque {
+	Node head;
+	int size;
+	class Node {
+		int data;
+		Node next;
+		Node(int n) {
+			this.data = n;
+		}
+	}
+	Steque() {
+
+	}
+	void push(int n) {
+		Node newnode = new Node(n);
+		if(size == 0) {
+			head = newnode;
+			head.next = null;
+
+		} else {
+			newnode.next = head;
+			newnode = head;
+
+		}
+		size++;
+		print();
+	}
+	void enqueue(int n) {
+		Node newnode = new Node(n);
+		if(size == 0) {
+			head = newnode;
+			head.next = null;
+
+		} else {
+			Node thead = head;
+			while (thead.next != null) {
+				thead = thead.next;
+			}
+			thead.next = newnode;
+ 		}
+ 		print();
+	}
+	void pop() {
+		Node thead = head.next;
+		head = thead;
+		print();
+	}
+	void print() {
+		Node thead = head;
+		while(thead.next.next != null) {
+			System.out.println(thead.data + ",");
+			thead = thead.next;
+		}
+		System.out.println(thead.next.data);
+	}
+}
